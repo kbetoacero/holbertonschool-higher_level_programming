99a480ae4ea55d175066236c4f338a1c0cf98fdf
vagrant <vagrant@vagrant-ubuntu-trusty-64.com>
2020-09-01 23:15:31 +0000

    1-last_digit.py fixed2

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,10 +8,10 @@ if number < 0:
     last = last * -1
 if last > 5:
     print("Last digit of {} is {} and is greater than 5"
-	.format(number, last))
+	  .format(number, last))
 elif last == 0:
     print("Last digit of {} is {} and is 0"
-	.format(number, last))
+	  .format(number, last))
 else:
     print("Last digit of {} is {} and is less than 6 and not 0"
-	.format(number, last))
+	  .format(number, last))
