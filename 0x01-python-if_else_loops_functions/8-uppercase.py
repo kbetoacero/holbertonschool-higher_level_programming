935cfddc2f626a9707f5b104d472275c11d8e48f
vagrant <vagrant@vagrant-ubuntu-trusty-64.com>
2020-09-02 00:32:05 +0000

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
+
     for l in range(len(str)):
         letter = ord(str[l])
         if (letter >= 97 and letter <= 122):
-           letter -= 32
+            letter -= 32
         print("{}".format(chr(letter)), end="")
     print()
