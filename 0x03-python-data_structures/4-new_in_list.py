0c32e4ed6ecf1a409bc772f6ed6f68a85ed97a0c
vagrant <vagrant@vagrant-ubuntu-trusty-64.com>
2020-09-08 19:49:46 +0000

    4-new_in_list.py code fix2

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,4 +3,4 @@ def new_in_list(my_list, idx, element):
     my_list_copy = my_list.copy()
     if -1 < idx < len(my_list):
         my_list_copy[idx] = element
-        return copy_my_list
+        return my_list_copy
