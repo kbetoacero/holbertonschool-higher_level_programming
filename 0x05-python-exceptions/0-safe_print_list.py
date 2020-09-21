818b9a6d73434031fa15f790449ba9fb2bf56c16
vagrant <2028@holbertonschool.com>
2020-09-21 23:47:12 +0000

    0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# prints x elements of a list
+def safe_print_list(my_list=[], x=0):
+    try:
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
+        print()
+        return x
+    except IndexError:
+        print()
+        return i
