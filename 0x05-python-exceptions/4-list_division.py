a953a15f0540cc5651a744c689e9eec3567c6667
vagrant <2028@holbertonschool.com>
2020-09-22 00:01:21 +0000

    4-list_division.py fixed

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -14,6 +14,6 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             print("out of range")
         finally:
-            new.append(div_list)
+            new_list.append(div_list)
 
     return new_list
