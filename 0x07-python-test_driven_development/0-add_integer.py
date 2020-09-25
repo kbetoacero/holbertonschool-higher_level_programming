9d57ee91041af35c9f852518deb594783732d525
vagrant <2028@holbertonschool.com>
2020-09-25 23:04:12 +0000

    0-add_integer.py and testfile

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+""" function to add 2 integers """
+
+def add_integer(a, b=98):
+
+    """ adds two integers a and b """
+
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    elif type(a) is float:
+        a = int(a)
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    elif type(b) is float:
+        b = int(b)
+
+    return a + b
