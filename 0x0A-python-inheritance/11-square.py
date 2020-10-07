d956677906205b019559d8ebc05d9b37a50112ab
kbetoacero <2028@holbertonschool.com>
2020-10-07 02:14:15 +0000

    11-square.py pycodestyle fixed

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -7,7 +7,7 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     """Square class"""
-    
+
     def __init__(self, size):
         """ instantiating square
         """
