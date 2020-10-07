02f133ad1ea8751cbba5119c274c2baad093b42a
kbetoacero <2028@holbertonschool.com>
2020-10-07 01:04:31 +0000

    1-my_list.py code fixed

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,9 +7,8 @@ class MyList(list):
     """ MyList that inherits
      from list """
 
-    def __init__(self):
-        super().__init__(self)
-
     def print_sorted(self):
-        self.list = self
-        print(sorted(self.list))
+        """ Print List of int
+        ascending order """
+
+        print(sorted(self))
