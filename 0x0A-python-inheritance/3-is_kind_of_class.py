6ae6f7e2717284594c3a3940f3fafededeeb6fe5
kbetoacero <2028@holbertonschool.com>
2020-10-07 01:36:20 +0000

    3-is_kind_of_class.py pydecostyle

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
 Module to test an obj to see which class
-it belongs to or is inherited 
+it belongs to or is inherited
 """
 
 
 def is_kind_of_class(obj, a_class):
-    """ True is obj is an instance or 
+    """ True is obj is an instance or
     inherited from a_class. False otherwise
     """
     if isinstance(obj, a_class):
