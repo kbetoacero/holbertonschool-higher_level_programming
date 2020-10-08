fb08b3abc2410163243968fca7d6271b7ff481ce
kbetoacero <2028@holbertonschool.com>
2020-10-08 01:26:07 +0000

    9-add_item.py pep8 fixed

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """ Load, add, save """
+import sys
+
 
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-import sys
 
 try:
     fde = load_from_json_file("add_item.json")
