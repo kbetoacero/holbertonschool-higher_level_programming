6967cf528ff3607b748aaa344561520fbe910bb8
kbetoacero <2028@holbertonschool.com>
2020-10-08 01:02:42 +0000

    6-from_json_string.py

---
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" From JSON string to
+    object """
+
+import json
+
+
+def from_json_string(my_str):i
+    """ returns an object (Python data structure)
+    represented by a JSON string """
+    return json.loads(my_str)
