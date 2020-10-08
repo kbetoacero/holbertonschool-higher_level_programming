dd5878cbed7392275c53a054d2e6e271c8983119
kbetoacero <2028@holbertonschool.com>
2020-10-08 01:03:43 +0000

    6-from_json_string.py fixed

--- 0x0B-python-input_output/6-from_json_string.py
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -5,7 +5,7 @@
 import json
 
 
-def from_json_string(my_str):i
+def from_json_string(my_str):
     """ returns an object (Python data structure)
     represented by a JSON string """
     return json.loads(my_str)
