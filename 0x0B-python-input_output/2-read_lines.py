6db75a556b63b59f0f97995544ab14e6bfd1f414
kbetoacero <2028@holbertonschool.com>
2020-10-08 00:37:49 +0000

    2-read_lines.py

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Read n lines """
+
+
+def read_lines(filename="", nb_lines=0):
+    """ reads n lines of a text file
+    (UTF8) and prints it to stdout """
+    with open(filename, 'w', encoding="utf8") as textfile:
+        fde = textfile.write(text)
+    return fde
