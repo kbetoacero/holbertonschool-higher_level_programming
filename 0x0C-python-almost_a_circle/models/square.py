a573f52b0ea643e07aa55ba081bbde4ade31a278
kbetoacero <2028@holbertonschool.com>
2020-10-14 18:28:47 +0000

    square.py code fix 2 __str__

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,9 +9,6 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """ Instatiation Square """
 
-        self.size = size
-        self.x = x
-        self.y = y
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
