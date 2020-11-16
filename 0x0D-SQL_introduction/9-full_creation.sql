a80b24785f8ffe8f5c00a123ae2c1af45278a4fc
kbetoacero <2028@holbertonschool.com>
2020-11-16 22:55:29 +0000

    9-full_creation.sql fixed2

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,9 +1,9 @@
 -- 9 Full creation
 
 CREATE TABLE IF NOT EXISTS second_table (
-		id INT,
-		name VARCHAR(256),
-		score INT);
+id INTEGER,
+name VARCHAR(256),
+score INTEGER);
 INSERT INTO second_table (id, name, score) VALUES (1, "Jhon", 10);
 INSERT INTO second_table (id, name, score) VALUES (2, "Alex", 3);
 INSERT INTO second_table (id, name, score) VALUES (3, "Bob", 14);
