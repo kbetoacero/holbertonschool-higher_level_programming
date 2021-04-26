f19c742c13494eda22573281bcbbf3021bbbaa29
Carlos Alberto Acero Pinzón <65452536+kbetoacero@users.noreply.github.com>
2021-04-26 08:47:59 -0500

    Delete 0x06-python-classes directory

--- 0x06-python-classes/1-square.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Script for Empty class Square that does nothing but a square """
-
-
-class Square:
-    """ private class method size,
-        instantiation and define a square """
-    def __init__(self, size):
-        self.__size = size
