1c253dfac6f15162154b529aaf05803935767829
Carlos Alberto Acero Pinzón <65452536+kbetoacero@users.noreply.github.com>
2021-04-26 08:47:19 -0500

    Delete 0x02-python-import_modules directory

--- 0x02-python-import_modules/3-infinite_add.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    arguments = len(sys.argv) - 1
-    x = 0
-    for arg in range(arguments):
-        x += int(sys.argv[arg + 1])
-    print("{}".format(x))
