fddbd449ecad774b200e28fd6aa56b4d57a00b7f
Carlos Alberto Acero Pinzón <65452536+kbetoacero@users.noreply.github.com>
2021-04-26 08:47:39 -0500

    Delete 0x04-python-more_data_structures directory

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-
-    square = [[i ** 2 for i in row] for row in matrix]
-    return square
--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/python3
-def complex_delete(a_dictionary, value):
