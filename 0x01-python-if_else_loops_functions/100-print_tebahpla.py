960ca4eb2d56f90193d139fa2a9b8b9d73577332
Carlos Alberto Acero Pinzón <65452536+kbetoacero@users.noreply.github.com>
2021-04-26 08:47:10 -0500

    Delete 0x01-python-if_else_loops_functions directory

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-for x in range(25, -1, -1):
-    print("{:s}".format(chr(x + ord('a')) if x % 2 else chr(x + ord('A'))),
-          end="")
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-for number in range(0, 100):
-    first = number / 10
-    last = number % 10
-    if first < last and number != 89:
-        print("{:02}, ".format(number), end="")
-    elif number == 89:
-        print("{}".format(number))
