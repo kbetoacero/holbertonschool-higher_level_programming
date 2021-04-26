f8be8ab7bfe397bdb8f0f12fe9a5be0d2d04ceb6
Carlos Alberto Acero Pinzón <65452536+kbetoacero@users.noreply.github.com>
2021-04-26 08:47:29 -0500

    Delete 0x03-python-data_structures directory

--- 0x03-python-data_structures/4-new_in_list.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def new_in_list(my_list, idx, element):
-    my_list_copy = my_list.copy()
-    if my_list_copy is None:
-        return
-    if -1 < idx < len(my_list_copy):
-        my_list_copy[idx] = element
-    return my_list_copy
--- 0x03-python-data_structures/5-no_c.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def no_c(my_string):
-    my_string2 = list(my_string)
-    for i in range(my_string2.count('c')):
-        my_string2.remove('c')
-    for i in range(my_string2.count('C')):
-        my_string2.remove('C')
-    str2 = ""
-    return str2.join(my_string2)
