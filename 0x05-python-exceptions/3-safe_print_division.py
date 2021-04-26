8ee462a921c0f121aebd5a26b30c6413fc66a68d
Carlos Alberto Acero Pinzón <65452536+kbetoacero@users.noreply.github.com>
2021-04-26 08:47:50 -0500

    Delete 0x05-python-exceptions directory

--- 0x05-python-exceptions/3-safe_print_division.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def safe_print_division(a, b):
-    try:
-        r = a / b
-    except ZeroDivisionError:
-        r = None
-    finally:
-        print("Inside result: {}".format(r))
-        return r
