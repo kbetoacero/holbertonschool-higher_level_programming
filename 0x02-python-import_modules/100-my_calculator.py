1fef33472b61ebe909ce7ea487d717399c02d39b
kbetoacero <carlos.acero@holbertonschool.com>
2021-04-28 22:43:31 +0000

    100-my_calculator.py corrected 4

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     from sys import argv
     option = "+-*/"
     if len(argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b")
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     elif argv[2] not in option:
         print("Unknown operator. Available operators: +, -, * and /")
