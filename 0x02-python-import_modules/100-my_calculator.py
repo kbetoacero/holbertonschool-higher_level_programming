a2b70bf5e34d636d0859337b66d952e4ecc0db20
kbetoacero <carlos.acero@holbertonschool.com>
2021-04-28 22:33:26 +0000

    100-my_calculator.py corrected 2

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -13,10 +13,10 @@ if __name__ == "__main__":
         a = int(argv[1])
         b = int(argv[3])
         if argv[2] == "+":
-            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+            print("{} + {} = {}".format(a, b, add(a, b)))
         elif argv[2] == "-":
-            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+            print("{} - {} = {}".format(a, b, sub(a, b)))
         elif argv[2] == "*":
             print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
         elif argv[2] == "/":
-            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+            print("{} / {} = {}".format(a, b, div(a, b)))
