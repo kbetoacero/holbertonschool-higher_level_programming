3bd122eb6413e28cfe923e298a5e6888b8ad847b
kbetoacero <carlos.acero@holbertonschool.com>
2021-06-01 01:12:30 +0000

    10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Student to JSON"""
+
+
+class Student:
+    """Student Class"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        else:
+            new_dict = {}
+            for item in attrs:
+                if hasattr(self, item):
+                    new_dict[item] = getattr(self, item)
+        return (new_dict)
