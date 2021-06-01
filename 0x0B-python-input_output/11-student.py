f467b5c2378aa64a442fef23a843a413e0bf4cfc
kbetoacero <carlos.acero@holbertonschool.com>
2021-06-01 01:38:02 +0000

    11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Student to JSON"""
+
+
+class Student:
+    """Student Class"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        else:
+            new_dict = {}
+            for item in attrs:
+                if hasattr(self, item):
+                    new_dict[item] = getattr(self, item)
+        return (new_dict)
+
+    def reload_from_json(self, json):
+        """ Function that replaces all atrribs
+        of a student instance"""
+        for key, value in json.items():
+            if "fist_name" in json:
+                self.first_name = json["first_name"]
+            if "last_name" in json:
+                self.last_name = json["last_name"]
+            if "age" in json:
+                self.age = json["age"]
