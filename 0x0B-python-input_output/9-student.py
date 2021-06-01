537a9fc84909717252abb9c31f2d4ba9ee244f5f
kbetoacero <carlos.acero@holbertonschool.com>
2021-06-01 00:43:04 +0000

    9-student.py

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Student to JSON"""
+
+
+class Student:
+    """Student Class"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        dict = self.__dict__
+        return dict
