ba0c2e6a45db0d6c3a9551e7b2eda8f4eee1ca76
kbetoacero <carlos.acero@holbertonschool.com>
2021-06-08 22:43:03 +0000

    rectangle corrected and square

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -9,14 +9,14 @@ class Rectangle(Base):
     """Class rectangle that inherits from Base"""
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
-        self.__width = width
+        self.width = width
         self.height = height
-        self.__x = x
+        self.x = x
         self.y = y
 
     @property
     def width(self):
-        return self.width
+        return self.__width
 
     @width.setter
     def width(self, value):
@@ -24,7 +24,7 @@ class Rectangle(Base):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must > 0")
-        self.width = value
+        self.__width = value
 
     @property
     def height(self):
@@ -40,7 +40,7 @@ class Rectangle(Base):
 
     @property
     def x(self):
-        return self.x
+        return self.__x
 
     @x.setter
     def x(self, value):
@@ -48,7 +48,7 @@ class Rectangle(Base):
             raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
-        self.x = value
+        self.__x = value
 
     @property
     def y(self):
@@ -65,7 +65,7 @@ class Rectangle(Base):
     def area(self):
         """ Returns the area value of the Rectangle
         instance """
-        return (self.__width * self.height)
+        return (self.__width * self.__height)
 
     def display(self):
         """ prints in stdout the Rectangle
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,3 +9,20 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
         self.size = size
+
+    @property
+    def size(self):
+        """getter for size"""
+        return self.__width
+
+    @size.setter
+    def size(self, value):
+        """setter for size attribute"""
+        self.__width = value
+        self.__height = value
+
+    def __str__(self):
+        """string representation of an instance"""
+        return "[{}] ({}) {}/{} - {}".format(self.__class__.__name__,
+                                             self.id, self.x,
+                                             self.y, self.size)
