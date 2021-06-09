09a28836bfd6a4fedd8d1cf2ca1667d9b985b8a3
kbetoacero <carlos.acero@holbertonschool.com>
2021-06-09 01:14:49 +0000

    models/base.py Jsoin string to dictionary

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -38,3 +38,12 @@ class Base:
                 new_list.append(cls.to_dictionary(obj))
         with open(file_name, mode='w', encoding='utf-8') as file:
             file.write(cls.to_json_string(new_list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """  returns the list of the JSON string
+        representation json_string"""
+        if json_string is None or len(json_string) == 0:
+            return []
+        else:
+            return json.loads(json_string)
