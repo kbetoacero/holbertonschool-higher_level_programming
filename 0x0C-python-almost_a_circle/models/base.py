f565bc773e4adf2f40d803216d7f4c2d807a1e4b
kbetoacero <carlos.acero@holbertonschool.com>
2021-06-09 00:31:03 +0000

    models/base.py Dictionary to Json string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,7 +2,11 @@
 """ Base Class """
 
 
+import json
+
+
 class Base:
+
     """ Base Class
     public attribute __nb_objects"""
     __nb_objects = 0
@@ -13,3 +17,12 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ return JSON reprsentation of list
+        dictionaries"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
