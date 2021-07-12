8b338cab62a437df2c9dc7ffc8cbbe2e14eaa19d
kbetoacero <carlos.acero@holbertonschool.com>
2021-07-12 23:39:17 +0000

    102-top_city.sql

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,4 @@
+-- displays the top 3 of cities temperature durung july and august order by temp
+SELECT city, AVG(value) AS avg_temp FROM temperatures
+WHERE month=7 or month=8 GROUP BY city ORDER BY avg_temp
+DESC LIMIT 3
